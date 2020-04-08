f7abdb643a7f424995bdf34272d102c3d342b19c
ivan <15207126400@163.com>
2020-04-08 10:04:10 +0800

    swagger 集成

--- linksame-crm/src/main/java/com/linksame/crm/common/minio/controller/MinioController.java
+++ linksame-crm/src/main/java/com/linksame/crm/common/minio/controller/MinioController.java
@@ -5,6 +5,12 @@ import com.jfinal.core.Controller;
 import com.jfinal.kit.LogKit;
 import com.jfinal.upload.UploadFile;
 import com.linksame.crm.common.minio.service.MinioServicce;
+import live.autu.plugin.jfinal.swagger.annotation.Api;
+import live.autu.plugin.jfinal.swagger.annotation.ApiImplicitParam;
+import live.autu.plugin.jfinal.swagger.annotation.ApiImplicitParams;
+import live.autu.plugin.jfinal.swagger.annotation.ApiOperation;
+import live.autu.plugin.jfinal.swagger.config.RequestMethod;
+
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileNotFoundException;
 import java.util.Map;
@@ -14,12 +20,16 @@ import java.util.Map;
  * @date 2020/4/6 15:42
  * @Description minio接口测试
  */
+@Api(description="Minio文件上传服务")
 public class MinioController extends Controller {
 
     /**
      * 上传文件
-     * @throws FileNotFoundException
      */
+    @ApiOperation(methods= RequestMethod.POST, description="上传文件")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="file", description="文件")
+    })
     public void uploadFile() throws FileNotFoundException {
         UploadFile file = getFile("file");
         Map<String, String> map = MinioServicce.uploadFile(file);
@@ -30,23 +40,31 @@ public class MinioController extends Controller {
     /**
      * 下载文件
      */
-    public void downloadFile(){
+    @ApiOperation(methods= RequestMethod.POST, description="下载文件")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="fileName", description="文件名")
+    })
+    public void downloadFile() throws Exception {
         String fileName = get("fileName");
         HttpServletResponse response = getResponse();
         MinioServicce.downloadFile(fileName, response);
+
+        renderNull();
     }
 
     /**
      * 获取图片路径
      */
+    @ApiOperation(methods= RequestMethod.POST, description="获取图片路径")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="fileName", description="文件名")
+    })
     public void getImgUrl() {
-        String objectName = get("objectName");
-        Integer outTime = getInt("outTime");
-        HttpServletResponse response = getResponse();
+        String fileName = get("fileName");
         //获取文件后缀
-        String suffix = FileUtil.extName(objectName);
+        String suffix = FileUtil.extName(fileName);
         LogKit.info("文件后缀: " + suffix);
-        String url = MinioServicce.getImgUrl(suffix, objectName, outTime, response);
+        String url = MinioServicce.getImgUrl(suffix, fileName);
 
         renderJson(url);
     }
--- linksame-crm/src/main/java/com/linksame/crm/erp/work/controller/RemindController.java
+++ linksame-crm/src/main/java/com/linksame/crm/erp/work/controller/RemindController.java
@@ -5,12 +5,18 @@ import com.jfinal.core.Controller;
 import com.jfinal.core.paragetter.Para;
 import com.linksame.crm.erp.work.entity.TaskRemind;
 import com.linksame.crm.erp.work.service.RemindService;
+import live.autu.plugin.jfinal.swagger.annotation.Api;
+import live.autu.plugin.jfinal.swagger.annotation.ApiImplicitParam;
+import live.autu.plugin.jfinal.swagger.annotation.ApiImplicitParams;
+import live.autu.plugin.jfinal.swagger.annotation.ApiOperation;
+import live.autu.plugin.jfinal.swagger.config.RequestMethod;
 
 /**
  * @author Ivan
  * @date 2020/4/1 12:47
  * @Description 任务提醒控制层
  */
+@Api(description="任务提醒服务")
 public class RemindController extends Controller {
     @Inject
     private RemindService remindService;
@@ -20,6 +26,10 @@ public class RemindController extends Controller {
      * 设置任务提醒
      * @param taskRemind 任务提醒对象
      */
+    @ApiOperation(methods=RequestMethod.POST, description="设置任务提醒")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="taskRemind", description="任务提醒对象")
+    })
     public void setRemind(@Para("") TaskRemind taskRemind){
         renderJson(remindService.setRemind(taskRemind));
     }
@@ -29,6 +39,10 @@ public class RemindController extends Controller {
      * 根据任务id查询任务提醒数据
      * @param taskId 任务编号
      */
+    @ApiOperation(methods=RequestMethod.POST, description="根据任务id查询任务提醒数据")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="taskId", description="任务编号")
+    })
     public void queryByTaskId(@Para("taskId")Integer taskId){
         renderJson(remindService.queryByTaskId(taskId));
     }
