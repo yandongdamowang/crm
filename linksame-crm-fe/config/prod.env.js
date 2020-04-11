20383b6a459db211eaab6afe7a15584da4d6ea6f
huangkejing <552318735@qq.com>
2020-04-11 10:46:05 +0800

    更新 请求接口

--- linksame-crm-fe/config/prod.env.js
+++ linksame-crm-fe/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  //   BASE_API: '"/"',
-  BASE_API: '"http://192.168.1.174:28080/"',
+  BASE_API: '"/api/"'
+  //   BASE_API: '"http://192.168.1.174:28080/"',
 }
